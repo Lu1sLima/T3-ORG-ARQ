1a8fb463ee5c2b7879c0cecf764628e3ade3339b
Luis Lima <luis.contato01@gmail.com>
2020-06-23 00:13:31 -0300

    enviando arquivo teste certo

--- files/teste2.asm
+++ files/teste2.asm
@@ -1,8 +1,10 @@
 .text
 .globl main
 main:
-    addiu $3, $3, $3
+    addu $2, $3, $7
     lw $3, 0($2)
+    addu $1 $10, $5
 .data
     a: .word 12 13 14 15
     b: .asciiz "Meu amigo, mas que loucura"
+    c: .word 123
