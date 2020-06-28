3c065a61902a881cb1a8a613e02068503e801e23
Luis Lima <luis.contato01@gmail.com>
2020-06-27 21:45:42 -0300

    adicionando arquivo de teste

--- files/teste.asm
+++ files/teste.asm
@@ -1,9 +1,21 @@
-.data
-    a: .word 12 13 14 15
-    b: .asciiz "Meu amigo, mas que loucura"
-    
 .text
 .globl main
 main:
-    addiu $3, $3, $3
-    lw $3, 0($2)
+    lui $1, 0x00001001
+    ori $10, $1, 0x00000000
+    lui $1, 0x00001001
+    ori $11, $1, 0x00000004
+    lw $10, 0x00000000($10)
+    lw $11, 0x00000000($11)
+    xor $7, $10, $11
+    and $8, $10, $11
+    andi $12, $10, 2
+    addu $9, $10, $11
+    addiu $10, $9, 10
+    lui $1, 0x00001001
+    ori $15, $1, 0x00000000
+    sw $10, 0($15)
+.data
+    A: .word	30
+    B: .word	5
+    c: .word 123
